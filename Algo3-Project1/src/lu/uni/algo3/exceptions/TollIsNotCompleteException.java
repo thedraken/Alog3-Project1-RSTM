de191afe117d4b3636ae84e210c3f18660bfaab5
thedraken <thedraken@gmail.com>
2015-05-20 22:32:32 +0200

    Update TollIsNotCompleteException.java
    Added annotation

--- Algo3-Project1/src/lu/uni/algo3/exceptions/TollIsNotCompleteException.java
+++ Algo3-Project1/src/lu/uni/algo3/exceptions/TollIsNotCompleteException.java
@@ -8,6 +8,7 @@ public class TollIsNotCompleteException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = -76015324339175821L;
+	//This message is thrown if a bill is attempted to be generate and the toll record hasn't been closed
 	public TollIsNotCompleteException(int id, Vehicle v){
 		_message = "The toll record " + id + " for vehicle " + v.getLicencePlate() + " cannot be have a bill generated, as it is not complete";
 	}
