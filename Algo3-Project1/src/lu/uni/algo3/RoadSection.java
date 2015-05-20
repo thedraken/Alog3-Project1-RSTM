2c77c4154cf7a0977bc4f8fc75f716de02f56be6
thedraken <thedraken@gmail.com>
2015-05-20 22:16:03 +0200

    Update RoadSection.java
    Added annotations

--- Algo3-Project1/src/lu/uni/algo3/RoadSection.java
+++ Algo3-Project1/src/lu/uni/algo3/RoadSection.java
@@ -34,6 +34,7 @@ public class RoadSection implements Comparable<RoadSection> {
 		this._connectionToOtherRoadSections = new HashSet<RoadSection>();
 		this._roadContinutesAfterSection = false;
 	}
+	//The base constructor called by both constructors, this will set the standard values needed in both
 	private void baseRoadSection(int speedLimit, int maxOccupation, double distanceOfRoadSection, Camera cam, Road roadIsWith) throws OutOfRangeException{
 		this._number = SQLIndexer.getInstance().getNewID(SQLType.RoadSection);
 		this._speedLimit = speedLimit;
@@ -64,46 +65,59 @@ public class RoadSection implements Comparable<RoadSection> {
 	public Road road(){
 		return this._road;
 	}
+	//A synced hashset of vehicles, has to be synced as this can change at any time
 	public synchronized  HashSet<Vehicle> getAllVehiclesInside(){
 		return this._listOfVehiclesInside;
 	}
+	//Returns a boolean confirming if the road has more motorway left or not to run on
 	public boolean roadContinutesAfterSection(){
 		return this._roadContinutesAfterSection;
 	}
+	//Returns a hashset of road sections this roadsection is connected to
 	public synchronized HashSet<RoadSection> connectionToOtherRoadSections(){
 		return this._connectionToOtherRoadSections;
 	}
 	public synchronized HashSet<RoadSectionObserver> listOfObservers(){
 		return this._listOfObservers;
 	}
+	//Gets the current number of vehicles inside the road section
 	public int getOccupation(){
-		return this._listOfVehiclesInside.size();
+		return this.getAllVehiclesInside().size();
 	}
 	public Camera getCamera(){
 		return this._camera;
 	}
+	//A method to insert a vehicle inside a road section
 	public synchronized void insertVehicle(Vehicle v) throws ExceedMaxOccupationException, ObjectExistsInCollectionException{
+		//First checks to make sure there is room in the roadsection
 		if (_listOfVehiclesInside.size() +1 > _maxOccupation)
 			throw new ExceedMaxOccupationException(_listOfVehiclesInside.size(), maxOccupation(), 1);
+		//Then makes sure the vehicle is not already in the road section
 		if (_listOfVehiclesInside.contains(v))
 			throw new ObjectExistsInCollectionException();
 		_listOfVehiclesInside.add(v);
 		try {
+			//Captures a photo of the car, since it is now in the road section
 			this._camera.capturePhoto(v);
 		} catch (OutOfRangeException e) {
 			System.out.println("Camera " + this._camera.iD() + " on the roadsection " + this.number() + " was unable to take a photograph");
 			e.printStackTrace();
 		}
+		//If the car is new to the system, it was generate a new toll record, else it will update the current one
 		if (v.getTollRecord() == null)
 			v.setTollRecord(new TollRecord(v, this));
 		else
 			v.getTollRecord().addRoadSection(this);
+		//Runs a method to update observers, if they need to know about the vehicles
 		notifyObservers();
 	}
+	//A synced method to remove a vehicle from this road section
 	public synchronized boolean removeVehicle(Vehicle v){
+		//Make sure the vehicle is actually in the road section
 		if (_listOfVehiclesInside.contains(v)){
 			_listOfVehiclesInside.remove(v);
 			notifyObservers();
+			//If the car is leaving the toll system, this will close the toll record
 			if (v.hasExitedRoadMap())
 				v.getTollRecord().setExit(this);
 			return true;
@@ -113,6 +127,7 @@ public class RoadSection implements Comparable<RoadSection> {
 	public List<Vehicle> getVehiclesByCategory(Category c){
 		return Predicates.filterVehicles(this.getAllVehiclesInside(), Predicates.isCategory(c));
 	}
+	//Returns true if a road is considered congested
 	public boolean isBusy(){
 		//Road is considered busy if over half occupation
 		int currentCarsOnRoad = this.getAllVehiclesInside().size();
@@ -133,6 +148,7 @@ public class RoadSection implements Comparable<RoadSection> {
 		for(RoadSectionObserver obs : _listOfObservers)
 			obs.updateRS(this);
 	}
+	//Adds a connection of a road section to another. This can include from other motorways, so acting like a junction
 	public synchronized void addRoadSectionConnection(RoadSection rs){
 		if (this._connectionToOtherRoadSections.contains(rs))
 			return;
