199d537ecd1b5489ceff16817108ed50ec80689b
thedraken <thedraken@gmail.com>
2015-05-20 22:30:54 +0200

    Update MissingVehicleException.java
    Added annotation

--- Algo3-Project1/src/lu/uni/algo3/exceptions/MissingVehicleException.java
+++ Algo3-Project1/src/lu/uni/algo3/exceptions/MissingVehicleException.java
@@ -5,7 +5,7 @@ public class MissingVehicleException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 5154622817227222650L;
-
+	//If attempting to access a list and it has no vehicles in it, this is thrown
 	@Override
 	public String getMessage(){
 		return "No vehicles were found in the list";
