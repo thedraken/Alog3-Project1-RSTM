47af9a5c973ed2ddef3ffb6d1732e923e9fe1cd3
thedraken <thedraken@gmail.com>
2015-05-20 22:29:36 +0200

    Update ExceedMaxOccupationException.java
    Added annotation

--- Algo3-Project1/src/lu/uni/algo3/exceptions/ExceedMaxOccupationException.java
+++ Algo3-Project1/src/lu/uni/algo3/exceptions/ExceedMaxOccupationException.java
@@ -6,6 +6,7 @@ public class ExceedMaxOccupationException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = -3659096395793882200L;
+	//If a roadsection exceeds it's maxiumum occupancy, this is custom error message is thrown
 	public ExceedMaxOccupationException(int currentNumber, int maximum, int numberToAdd){
 		_message = "The current number of items is " + currentNumber + " and the maximum is " + maximum + ", so cannot add " + numberToAdd;
 	}
