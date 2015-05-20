2d29765f1831a3696d48099f1445ec564e47ba84
thedraken <thedraken@gmail.com>
2015-05-20 22:31:20 +0200

    Update ObjectExistsInCollectionException.java
    Added annotation

--- Algo3-Project1/src/lu/uni/algo3/exceptions/ObjectExistsInCollectionException.java
+++ Algo3-Project1/src/lu/uni/algo3/exceptions/ObjectExistsInCollectionException.java
@@ -5,7 +5,7 @@ public class ObjectExistsInCollectionException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = -3810730536053223762L;
-
+	//If we attempt to add an object to a hashset, this exception is thrown
 	@Override
 	public String getMessage(){
 		return "Cannot add object to collection as already within collection";
